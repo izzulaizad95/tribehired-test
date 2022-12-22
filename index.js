5605b25d677d963e2b47cb3e92a7fae53665a659
izzulaizad95 <izzulaizad95@gmail.com>
2022-12-22 18:34:09 +0800

    init: gitignore file feature: api for top posts and filter posts

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/top", async (res, req) => {
+app.get("/posts/top", async (req, res) => {
   const posts = await getPosts();
   const comments = await getComments();
 
   let postIDtoCommentCount = {};
 
-  await comments.map((comment) => {
+  // populate obj with postId as key with total number of comments as value
+  comments.map((comment) => {
     if (comment.postId in postIDtoCommentCount) {
       postIDtoCommentCount[comment.postId]++;
     } else {
@@ -20,7 +21,8 @@ app.get("/top", async (res, req) => {
     }
   });
 
-  const formatPosts = await posts.map((post) => {
+  // format response and include total number of comments for respective posts
+  const formatPosts = posts.map((post) => {
     return {
       post_id: post.id,
       post_title: post.title,
@@ -29,13 +31,41 @@ app.get("/top", async (res, req) => {
     };
   });
 
-  const topPosts = await formatPosts.sort(
+  const topPosts = formatPosts.sort(
     (a, b) => a.total_number_of_comments - b.total_number_of_comments
   );
 
   res.send(topPosts);
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.get("/posts", async (req, res) => {
+  const comments = await getComments();
+
+  const filters = req.query;
+  const filteredComments = comments.filter((comment) => {
+    for (let key in filters) {
+      // convert param value to number if value is a number
+      const filterValue =
+        typeof comment[key] === "number"
+          ? key.toLowerCase().includes("id")
+            ? parseInt(filters[key])
+            : parseFloat(filters[key])
+          : filters[key];
+
+      // if any of the param value mismatch, filter out result
+      if (comment[key] !== filterValue) {
+        return false;
+      }
+    }
+    return true;
+  });
+
+  if (filteredComments.length === 0) {
+    res.status(404).json({ error: "No comments found" });
+  }
+  res.send(filteredComments);
+});
+
+app.listen(8888, () => {
+  console.log("Server is running on port 8888");
 });
